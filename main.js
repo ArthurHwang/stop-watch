c617e48a8365529f33d3d39c3a9f104a6abbd0b6
ArthurHwang <ahwang55@gmail.com>
2018-05-29 22:53:34 -0700

    Track whether the watch is currently running #2

--- main.js
+++ main.js
@@ -4,14 +4,25 @@ let $elapsedTime = document.getElementById('elapsed-time');
 let seconds = 00;
 let minutes = 00;
 let hours = 00;
+let interval;
 
 $startButton.addEventListener('click', (e) => {
-	let interval = setInterval(startTimer)
-	interval(startTimer, 1000)
+	interval = setInterval(startTimer);
+	if(interval) {
+		console.log("running")
+	} else {
+		console.log("not running")
+	}	
+	interval(startTimer, 1000);	
 });
 
-function startTimer () {
-		seconds++
+function startTimer() {
+		seconds++;
 		$elapsedTime.innerHTML = hours + ":" + minutes + ":" + seconds;
 }
 
+function pauseTimer() {
+	if (interval) {
+		console.log('setInterval Running')
+	}
+}
