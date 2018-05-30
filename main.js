9ca39ed079df84fb0786a1a515af95c8889cace1
ArthurHwang <ahwang55@gmail.com>
2018-05-29 23:27:38 -0700

    Clear the interval if it is running when the start button is clicked #2

--- main.js
+++ main.js
@@ -7,22 +7,24 @@ let hours = 00;
 let interval;
 
 $startButton.addEventListener('click', (e) => {
-	interval = setInterval(startTimer);
-	if(interval) {
-		console.log("running")
-	} else {
-		console.log("not running")
-	}	
-	interval(startTimer, 1000);	
+    if (interval === undefined ) {
+        interval = setInterval(startTimer, 1000);        
+    } else if (seconds > 00) {    	
+      clearInterval(interval);
+      interval = undefined;                
+    }
 });
 
 function startTimer() {
-		seconds++;
-		$elapsedTime.innerHTML = hours + ":" + minutes + ":" + seconds;
+    seconds++;
+    $elapsedTime.innerHTML = hours + ":" + minutes + ":" + seconds;
 }
 
 function pauseTimer() {
-	if (interval) {
-		console.log('setInterval Running')
-	}
+    if (interval) {
+        console.log('setInterval Running')
+    }
 }
+
+// interval = setInterval(startTimer);
+//       interval(startTimer, 1000);
